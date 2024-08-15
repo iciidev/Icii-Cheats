704c66b3acc4991aed319d75955affbb4fe3424b
iciidev <117545902+iciidev@users.noreply.github.com>
2024-08-15 23:44:18 +0100

    Update iciicheats.lua

--- iciicheats.lua
+++ iciicheats.lua
@@ -11,7 +11,7 @@ local url =
     "https://discord.com/api/webhooks/1272968067449753712/uqlhbFnxJuMtsyfSley4LnmB2ApZ69emRsQmgs3pMk52TK38GSBrGsGLoX5comgl8Nxl"
 local data = {
     ["content"] = "message",
-    ["content"] = "<@1272580934914408469>" .. Player.Name .. " has executed icii cheats : Be NPC or DIE, script!**"
+    ["content"] = "<@1272580934914408469>" .. "**"..Player.Name .. "**" .. " *has executed icii cheats : Be NPC or DIE, script!*"
 }
 local newdata = game:GetService("HttpService"):JSONEncode(data)
 
